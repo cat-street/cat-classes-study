06b2d4fcbe18338f41c61291b9e3c34f3aadf346
Andrey Kudryavtsev <lomastering@gmail.com>
2020-07-28 20:41:32 +0300

    feat: switching

---
+++ script.js
@@ -0,0 +1,78 @@
+class DOMHelper {
+  static moveElement(element, place) {
+    const newPlace = document.querySelector(place);
+    newPlace.append(element);
+  }
+
+  static clearEventListeners(element) {
+    const clone = element.cloneNode(true);
+    element.replaceWith(clone);
+    return clone;
+  }
+}
+
+class Tooltip {}
+
+class Cat {
+  constructor(id, element, moveFunction, type) {
+    this.id = id;
+    this.element = element;
+    this.moveFunction = moveFunction;
+    this.moveButtonInit(type);
+  }
+
+  infoButtonInit() {}
+
+  moveButtonInit(type) {
+    let moveButton = this.element.querySelector('.cat__move');
+    moveButton = DOMHelper.clearEventListeners(moveButton);
+    moveButton.textContent = type === 'shelter' ? 'To Vet' : 'To Shelter';
+    moveButton.addEventListener('click', this.moveFunction.bind(null, this.id));
+  }
+
+  update(updatedFunction, type) {
+    this.moveFunction = updatedFunction;
+    this.moveButtonInit(type);
+  }
+}
+
+class CatList {
+  constructor(type) {
+    this.cats = [];
+    this.type = type;
+    const selectedCats = document.querySelectorAll(
+      `.cats_type_${this.type} .cat`
+    );
+    selectedCats.forEach((element) => {
+      this.cats.push(
+        new Cat(element.id, element, this.moveCat.bind(this), this.type)
+      );
+    });
+  }
+
+  assignMoveFunction(moveFunction) {
+    this.moveFunction = moveFunction;
+  }
+
+  addCat(cat) {
+    this.cats.push(cat);
+    DOMHelper.moveElement(cat.element, `.cats_type_${this.type}`);
+    cat.update(this.moveCat.bind(this), this.type);
+  }
+
+  moveCat(catId) {
+    this.moveFunction(this.cats.find((element) => element.id === catId));
+    this.cats = this.cats.filter((element) => element.id !== catId);
+  }
+}
+
+class CatApp {
+  static init() {
+    const shelterCatsList = new CatList('shelter');
+    const vetCatsList = new CatList('vet');
+    shelterCatsList.assignMoveFunction(vetCatsList.addCat.bind(vetCatsList));
+    vetCatsList.assignMoveFunction(shelterCatsList.addCat.bind(shelterCatsList));
+  }
+}
+
+CatApp.init();
